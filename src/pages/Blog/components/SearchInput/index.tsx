53c83c859d964c5316a9cbf6ee394d3a99deeefb
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-15 15:00:34 -0300

    criando a função de search do blog

--- src/pages/Blog/components/SearchInput/index.tsx
+++ src/pages/Blog/components/SearchInput/index.tsx
@@ -1,13 +1,35 @@
+import { useForm } from 'react-hook-form'
 import { SearchInputContainer } from './styles'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export function SearchInput() {
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+interface SearchInputProps {
+  postsLength: number
+  getPosts: (query?: string) => Promise<void>
+}
+
+type SearchFormInput = z.infer<typeof searchFormSchema>
+
+export function SearchInput({ postsLength, getPosts }: SearchInputProps) {
+  const { register, handleSubmit } = useForm<SearchFormInput>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchPosts(data: SearchFormInput) {
+    await getPosts(data.query)
+  }
+
   return (
-    <SearchInputContainer>
+    <SearchInputContainer onSubmit={handleSubmit(handleSearchPosts)}>
       <header>
         <h3>Publicações</h3>
-        <span>6 publicações</span>
+        <span>{postsLength} publicações</span>
       </header>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input type="text" placeholder="Buscar conteúdo" {...register('query')} />
     </SearchInputContainer>
   )
 }
