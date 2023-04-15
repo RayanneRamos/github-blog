8f5d87a5de6f9e1ce4d7dc422c82bd7dd04464da
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-15 15:42:51 -0300

    deixando o PostHeader dinâmico

--- src/pages/Post/components/PostHeader/index.tsx
+++ src/pages/Post/components/PostHeader/index.tsx
@@ -8,9 +8,18 @@ import {
   faChevronLeft,
 } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from 'react-router-dom'
+import { IPost } from '../../../Blog'
+import { Spinner } from '../../../../components/Spinner'
+import { relativeDateFormatter } from '../../../../utils/formatter'
 
-export function PostHeader() {
+interface PostHeaderProps {
+  postData: IPost
+  isLoading: boolean
+}
+
+export function PostHeader({ postData, isLoading }: PostHeaderProps) {
   const navigate = useNavigate()
+  const formattedDate = relativeDateFormatter(postData?.created_at)
 
   function goBack() {
     navigate(-1)
@@ -18,30 +27,41 @@ export function PostHeader() {
 
   return (
     <PostHeaderContainer>
-      <header>
-        <ExternalLink
-          as="button"
-          icon={<FontAwesomeIcon icon={faChevronLeft} />}
-          text="Voltar"
-          onClick={goBack}
-          variant="iconLeft"
-        />
-        <ExternalLink text="Ver no Github" href="#" target="_blank" />
-      </header>
-      <h1>Javascript data types and data structures</h1>
-      <ul>
-        <li>
-          <FontAwesomeIcon icon={faGithub} />
-          RayanneRamos
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCalendar} />
-          Há 1 dia
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faComment} /> 5 comentários
-        </li>
-      </ul>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <header>
+            <ExternalLink
+              as="button"
+              icon={<FontAwesomeIcon icon={faChevronLeft} />}
+              text="Voltar"
+              onClick={goBack}
+              variant="iconLeft"
+            />
+            <ExternalLink
+              text="Ver no Github"
+              href={postData.html_url}
+              target="_blank"
+            />
+          </header>
+          <h1>{postData.title}</h1>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+              {postData.user.login}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCalendar} />
+              {formattedDate}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComment} /> {postData.comments}{' '}
+              comentários
+            </li>
+          </ul>
+        </>
+      )}
     </PostHeaderContainer>
   )
 }
--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,9 +1,36 @@
+import { useCallback, useEffect, useState } from 'react'
 import { PostHeader } from './components/PostHeader'
+import { IPost } from '../Blog'
+import { api } from '../../lib/api'
+import { useParams } from 'react-router-dom'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
 
 export function Post() {
+  const [postData, setPostData] = useState<IPost>({} as IPost)
+  const [isLoading, setIsLoading] = useState(true)
+  const { id } = useParams()
+
+  const getPostDetails = useCallback(async () => {
+    try {
+      setIsLoading(true)
+      const response = await api.get(
+        `/repos/${username}/${reponame}/issues/${id}`,
+      )
+      setPostData(response.data)
+    } finally {
+      setIsLoading(false)
+    }
+  }, [postData])
+
+  useEffect(() => {
+    getPostDetails()
+  }, [])
+
   return (
     <>
-      <PostHeader />
+      <PostHeader postData={postData} isLoading={isLoading} />
     </>
   )
 }
