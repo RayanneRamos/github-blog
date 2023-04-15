5252f742d846f67b2855549b4d812083f3a2443e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-15 11:51:01 -0300

    configurando o DefaultLayout nas rotas da aplicação

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import { BlogPage } from './pages/Blog'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<BlogPage />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<BlogPage />} />
+      </Route>
     </Routes>
   )
 }
