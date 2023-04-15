3ef474f5bc916e74b7e311ca5d55b7a974e8c157
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-15 12:43:48 -0300

    criando o PostListContainer no arquivo Blog

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,9 +1,13 @@
 import { Profile } from './components/Profile'
+import { SearchInput } from './components/SearchInput'
+import { PostListContainer } from './styles'
 
 export function BlogPage() {
   return (
     <>
       <Profile />
+      <SearchInput />
+      <PostListContainer></PostListContainer>
     </>
   )
 }
