b2fb41906723226a043cb39befd8a4f253665e43
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-15 14:49:44 -0300

    criando a função para trazer os posts

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,20 +1,57 @@
+import { useCallback, useEffect, useState } from 'react'
 import { Post } from './components/Post'
 import { Profile } from './components/Profile'
 import { SearchInput } from './components/SearchInput'
 import { PostListContainer } from './styles'
+import { api } from '../../lib/api'
+
+const username = import.meta.env.VITE_GITHUB_USERNAME
+const reponame = import.meta.env.VITE_GITHUB_REPONAME
+
+export interface IPost {
+  title: string
+  body: string
+  created_at: string
+  number: number
+  html_url: string
+  comments: number
+  user: {
+    login: string
+  }
+}
 
 export function BlogPage() {
+  const [posts, setPosts] = useState<IPost[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getPosts = useCallback(
+    async (query: string = '') => {
+      try {
+        setIsLoading(true)
+        const response = await api.get(
+          `/search/issues?q=${query}%20repo:${username}/${reponame}`,
+        )
+
+        setPosts(response.data.items)
+      } finally {
+        setIsLoading(false)
+      }
+    },
+    [posts],
+  )
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <>
       <Profile />
       <SearchInput />
       <PostListContainer>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post) => {
+          return <Post key={post.number} post={post} />
+        })}
       </PostListContainer>
     </>
   )
