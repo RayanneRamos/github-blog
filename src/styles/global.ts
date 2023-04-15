8ac0754cbd47989c617756d31549da21981e4fd6
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-15 11:44:35 -0300

    criando o arquivo  de estilização global da aplicação

---
+++ src/styles/global.ts
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+
+    ::-webkit-scrollbar {
+      width: 0.45rem;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: ${(props) => props.theme.colors['base-border']};
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: ${(props) => props.theme.colors['brand-blue']};
+      border-radius: 999px;
+    }
+  }
+
+  body {
+    background: ${(props) => props.theme.colors['base-background']};
+    color: ${(props) => props.theme.colors['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-size: ${(props) => props.theme.textSizes['text-text-m']};
+    font-weight: 400;
+    line-height: 160%;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
